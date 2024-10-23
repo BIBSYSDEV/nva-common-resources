4843d9e29cb2a9a44b91f8f42f4f2d8520bad327
Ketil Aasarød <ketil.aasarod@sikt.no>
2024-10-23 09:02:09 +0200

    initial version of a lambda function that deletes old lambda version every week

---
+++ delete_old_lambda_versions.py
@@ -0,0 +1,17 @@
+import boto3
+
+client = boto3.client('lambda', region_name='eu-west-1')
+
+functions_paginator = client.get_paginator('list_functions')
+version_paginator = client.get_paginator('list_versions_by_function')
+
+for function_page in functions_paginator.paginate():
+    for function in function_page['Functions']:
+        aliases = client.list_aliases(FunctionName=function['FunctionArn'])
+        alias_versions = [alias['FunctionVersion'] for alias in aliases['Aliases']]
+        for version_page in version_paginator.paginate(FunctionName=function['FunctionArn']):
+            for version in version_page['Versions']:
+                arn = version['FunctionArn']
+                if version['Version'] != function['Version'] and version['Version'] not in alias_versions:
+                    print('  🥊 {}'.format(arn))
+                    client.delete_function(FunctionName=arn)
